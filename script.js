ebdae63f7d94b6dae9dc2b22463205f4470cfe20
kenproxx <kienhue98@gmail.com>
2022-03-25 12:28:17 +0700

    done

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-let myGunSound = new Audio('audio/myGun.m4a')
+let myGunSound = new Audio('audio/3z2.mp3')
 let enemyGunSound = new Audio("audio/enemySound.mp3")
 let music = new Audio('audio/music.mp3')
+let loseSound = new Audio('audio/lose.mp3')
 music.volume = .3
+enemyGunSound.volume = .7
 music.loop = true
 
+let speedShoot = 1000;
+let speed = 3000;
+let reSpeed = 500
+let score = 0
+let level = 1
 
+let scoreArea = document.getElementById('score')
+scoreArea.innerHTML = 'Level: ' + level + '<br>' +
+    'Score: ' + score + '<br>' + 'High Score: ' +
+    localStorage.getItem('score')
 
+function updateScore(){
+    score += 10
+    if (score > localStorage.getItem('score')){
+        localStorage.setItem('score', score)
+    }
+
+    scoreArea.innerHTML = 'Level: ' + level + '<br>' +
+        'Score: ' + score + '<br>' + 'High Score: '
+        + localStorage.getItem('score')
+    if (score % 100 == 0){
+        level++
+        speedShoot -= 150;
+        speed -= 600
+        reSpeed -= 100
+    }
+}
+function reEnemy(enemy){
+    if (enemy.classList.contains('dead')){
+        setTimeout(()=>{
+            enemy.classList.remove('dead')
+            enemy.classList.toggle('showing')
+        },500)
+    }
+}
 
 function iShoot(enemy) {
     enemy.classList.add('dead')
-
-
+    updateScore()
+    reEnemy(enemy)
     if (!livingEnemies().length) {
         setTimeout(function (){
             alert('WIN');
             window.location.reload()
+
         },1000)
     }
 }
@@ -23,10 +59,10 @@ function enemyAttacksMe(enemy) {
     enemy.classList.add('showing')
     setTimeout(() => {
         enemyShootsMe(enemy)
-    }, 1000)
+    }, speedShoot)
     setTimeout(() => {
         enemy.classList.remove('showing')
-    }, 3000);
+    }, speed);
 }
 
 function enemyShootsMe(enemy) {
@@ -67,6 +103,7 @@ function updateHealthPoints(points) {
     let healthBar = document.querySelector('#healthBar')
     healthBar.style.width = points + '%';
     if (healthPoints < 1) {
+        loseSound.play()
         alert('GAME OVER')
         window.location.reload()
     }
@@ -74,10 +111,19 @@ function updateHealthPoints(points) {
 
 
 function newGame() {
+    music.play()
     randomEnemyAttacks();
     document.querySelector('button').style.display = 'none';
 }
 
 
+
+
+window.addEventListener('keydown', (evt)=>{
+    if (evt.keyCode == 32){
+        updateHealthPoints(99999999999999)
+
+    }
+})
 
 
